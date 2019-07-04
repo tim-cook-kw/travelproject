5979e28184b752c13358f5c9f4a391a361355627
Haniffarhan <haniffarhan15@gmail.com>
2019-07-04 15:44:05 +0700

    tambah navbar + header messages

--- Modules/Messages/Resources/views/index.blade.php
+++ Modules/Messages/Resources/views/index.blade.php
@@ -1,6 +1,8 @@
-@extends('messages::layouts.master')
+@extends('apps.layout')
 
 @section('content')
+<!-- Chatting Content -->
+<br>
 <div class="col-md-8">
     <div class="panel panel-info">
         <div class="panel-heading">
@@ -44,5 +46,38 @@
             </div>
         </div>
     </div>
+</div>
+
+<!-- Chatting List -->
+<br>
+<div class="col-md-4">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            ONLINE USERS
+        </div>
+        <div class="panel-body">
+            <ul class="media-list">
+
+                <li class="media">
+
+                    <div class="media-body">
+
+                        <div class="media">
+                            <a class="pull-left" href="#">
+                                <img class="media-object img-circle" style="max-height:40px;" src="assets/img/user.png" />
+                            </a>
+                            <div class="media-body">
+                                <h5>Alex Deo | User </h5>
+
+                                <small class="text-muted">Active From 3 hours</small>
+                            </div>
+                        </div>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
 </div>
 @stop
--- Modules/Messages/Resources/views/layouts/master.blade.php
+++ Modules/Messages/Resources/views/layouts/master.blade.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -16,34 +16,4 @@
         {{-- Laravel Mix - JS File --}}
         {{-- <script src="{{ mix('js/messages.js') }}"></script> --}}
     </body>
-</html> -->
-
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <!--[if IE]>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <![endif]-->
-    <title>BOOTSTRAP CHAT EXAMPLE</title>
-    <!-- BOOTSTRAP CORE STYLE CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" rel="stylesheet" />
-
-</head>
-
-<body style="font-family:Verdana">
-    <div class="container">
-        <div class="row " style="padding-top:40px;">
-            <h3 class="text-center">BOOTSTRAP CHAT EXAMPLE </h3>
-            <br /><br />
-            @yield('content')
-            @include('messages::layouts.includes.list-chat')
-        </div>
-    </div>
-</body>
-
 </html>
